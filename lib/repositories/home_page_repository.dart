2e7d2eb37bfbf599cc97eec932aac9f967faa61e
Fauzan Osama <atthiyya@gmail.com>
2022-12-02 10:15:37 +0700

    adding parameters endpoint

--- lib/repositories/home_page_repository.dart
+++ lib/repositories/home_page_repository.dart
@@ -5,11 +5,17 @@ import 'package:http/http.dart' as http;
 import '../models/product_response.dart';
 
 class HomePageRepository {
-  static const String _baseUrl =
-      'https://market-final-project.herokuapp.com/';
+  static const String _baseUrl = 'https://market-final-project.herokuapp.com/';
 
   Future<List<ProductResponse>> getProducts() async {
-    final response = await http.get(Uri.parse('${_baseUrl}buyer/product'));
+    final queryParameters = {
+      'status': 'available',
+      'page': '1',
+      'per_page': "30"
+    };
+
+    final response = await http.get(Uri.parse('${_baseUrl}buyer/product')
+        .replace(queryParameters: queryParameters));
 
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body);
