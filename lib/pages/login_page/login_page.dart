8b361981b3e39fa4fd959c2a9c6df972de95a629
Fauzan Osama <atthiyya@gmail.com>
2022-12-04 17:12:11 +0700

    create auth pages

--- lib/pages/login_page/login_page.dart
+++ lib/pages/login_page/login_page.dart
@@ -1,10 +1,42 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text('Login'),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                  labelText: 'Email', icon: Icon(CupertinoIcons.mail)),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                  labelText: 'Password', icon: Icon(CupertinoIcons.location)),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Start Bargain"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/register_page/register_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Register Page'),
+    );
+  }
+}
