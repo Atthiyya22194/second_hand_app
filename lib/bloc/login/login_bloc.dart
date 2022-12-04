679f1b62f42c55e264ea314475abf9f4f1dd8c36
Fauzan Osama <atthiyya@gmail.com>
2022-12-04 19:24:50 +0700

    store access token in shared preference

--- lib/bloc/login/login_bloc.dart
+++ lib/bloc/login/login_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:second_hand_app/bloc/login/login_events.dart';
 import 'package:second_hand_app/bloc/login/login_states.dart';
 import 'package:second_hand_app/repositories/auth_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final AuthRepository authRepository;
@@ -12,6 +13,8 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       try {
         final result = await authRepository.getProducts(
             email: event.email, password: event.password);
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('accessToken', result.accessToken);
         emit(LoginSuccessState(result));
       } catch (e) {
         emit(LoginErrorState(e.toString()));
